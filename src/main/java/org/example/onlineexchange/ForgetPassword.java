e3f0d0f1dd096e6f19ab953456e817a425cd0713
mitrape <miti1383@gmail.com>
2024-05-31 19:17:48 +0330

    add method to send email with a code

--- src/main/java/org/example/onlineexchange/ForgetPassword.java
+++ src/main/java/org/example/onlineexchange/ForgetPassword.java
@@ -5,8 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.Random;
 
 import java.io.IOException;
+import java.util.Properties;
 
 public class ForgetPassword {
     @FXML
@@ -37,11 +43,65 @@ public class ForgetPassword {
     private Label MessageReceivedCode;
     @FXML
     private Button ResendCode;
-    public void ClickOnLoginButton (ActionEvent event) throws IOException {
 
+    static boolean emailSent;
+    static Integer code;
+
+    public boolean sendEmail (String recipientEmail){
+        final String username = "onlineexchangedandp@gmail.com";
+        final String password = "aakhpnjipafgxuiw";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(prop,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("onlineexchangedandp@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(recipientEmail)
+            );
+            message.setSubject("log in with code");
+            Random rand = new Random();
+            code = rand.nextInt(10000);
+            message.setText(code.toString());
+
+            Transport.send(message);
+            return true;
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void ClickOnLoginButton (ActionEvent event) throws IOException {
+        String enteredCode = EnterCode.getText();
+        if(enteredCode.equals(code.toString())){
+            //بره صفحه های بعدی
+        }
+        else{
+            // نشون بده غلطه
+        }
     }
     public void ClickOnSubmitButton (ActionEvent event) throws IOException {
-
+        String email = EnterEmail.getText();
+        // بره ببینه همچین جییملی هست یا نه
+        emailSent = sendEmail(email);
+        if(emailSent){
+            //باید تمام دکمه های جدید رو نمایان کنه
+        }
     }
     public void ClickOnExit (ActionEvent event) throws IOException {
         System.exit(0);
