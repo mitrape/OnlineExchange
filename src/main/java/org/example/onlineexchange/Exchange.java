e5ca67ec532f0637fd8b83738d18330f36886c04
mitrape <miti1383@gmail.com>
2024-06-23 12:53:57 +0330

    few things are added to exchange

--- src/main/java/org/example/onlineexchange/Exchange.java
+++ src/main/java/org/example/onlineexchange/Exchange.java
@@ -8,8 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.*;
 import org.w3c.dom.Text;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class Exchange implements Initializable {
@@ -23,6 +28,12 @@ public class Exchange implements Initializable {
     private TextField PriceTextField;
     @FXML
     private TextField TurnoverTextField;
+
+    @FXML
+    private TableView <Transaction> userTable;
+    @FXML
+    private TableView <Transaction> marketTable;
+
     @FXML
     private TableColumn<Transaction, Double> userPrice;
     @FXML
@@ -63,6 +74,55 @@ public class Exchange implements Initializable {
         System.exit(0);
     }
     public void ClickOnDone (ActionEvent event){
+
+    }
+    public void setTable () throws SQLException {
+        userPrice.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("OpenRequests"));
+        userTurnover.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("amount"));
+        userTransactoinType.setCellValueFactory(new PropertyValueFactory<Transaction, String>("SellOrBuy"));
+
+        Price.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("OpenRequests"));
+        Turnover.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("amount"));
+        TransactoinType.setCellValueFactory(new PropertyValueFactory<Transaction, String>("SellOrBuy"));
+
+        if(CurrencyButton.getText().equals("TOMAN")){
+            Transaction transaction = null;
+            Statement statement1 = Main.connection.createStatement();
+            ResultSet resultSet1 = statement1.executeQuery("SELECT * FROM tomanopenrequests");
+            while (resultSet1.next()) {
+                Double OpenRequest = resultSet1.getDouble("tomanOpenRequests");
+                Double amount = resultSet1.getDouble("amount");
+                String SellOrBuy = resultSet1.getString("SellOrBuy");
+                transaction = new Transaction(OpenRequest, amount, SellOrBuy);
+                marketTable.getItems().add(transaction);
+            }
+
+            PreparedStatement statement = Main.connection.prepareStatement("SELECT * FROM tomantransaction WHERE username = ?");
+            statement.setString(1,Main.username);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                Double OpenRequest = resultSet1.getDouble("Transaction");
+                Double amount = resultSet1.getDouble("amount");
+                String SellOrBuy = resultSet1.getString("SellOrBuy");
+                transaction = new Transaction(OpenRequest, amount, SellOrBuy);
+                userTable.getItems().add(transaction);
+            }
+
+
+        }
+        else if (CurrencyButton.getText().equals("USD")){
+
+        }
+        else if (CurrencyButton.getText().equals("YEN")){
+
+        }
+        else if (CurrencyButton.getText().equals("EUR")){
+
+        }
+        else{
+            //GBP
+
+        }
 
     }
 }
