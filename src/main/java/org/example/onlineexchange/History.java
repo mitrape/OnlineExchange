7fd74c00efe96dac952133ba9981abf6639d3798
mitrape <miti1383@gmail.com>
2024-06-30 00:54:50 +0330

    first design and methods of history

---
+++ src/main/java/org/example/onlineexchange/History.java
@@ -0,0 +1,45 @@
+package org.example.onlineexchange;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+
+import java.io.IOException;
+
+public class History {
+    @FXML
+    private MenuButton PendingMenuButton ;
+    @FXML
+    private MenuButton TransactionMenuButton ;
+
+    public void ClickOnCVSTransaction(ActionEvent event) {
+
+    }
+
+    public void ClickOnJSONTransaction(ActionEvent event) {
+    }
+
+    public void ClickOnXMLTransaction(ActionEvent event) {
+    }
+
+    public void ClickOnExclTransaction(ActionEvent event) {
+    }
+
+    public void ClickOnSQLTransaction(ActionEvent event) {
+    }
+
+    public void ClickOnCVSPending(ActionEvent event) {
+    }
+
+    public void ClickOnJSONPending(ActionEvent event) {
+    }
+
+    public void ClickOnXMLPending(ActionEvent event) {
+    }
+
+    public void ClickOnExclPending(ActionEvent event) {
+    }
+
+    public void ClickOnSQLPending(ActionEvent event) {
+    }
+}
