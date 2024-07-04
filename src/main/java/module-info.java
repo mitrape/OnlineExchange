2f6e903e6b1108b1ee868b1880f1d8b5df45eeb0
mitrape <miti1383@gmail.com>
2024-07-04 13:54:33 +0330

    add few things

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module org.example.onlineexchange {
     requires java.mail ;
     requires commons.math3;
     requires java.desktop;
+    requires java.scripting;
 
     opens org.example.onlineexchange to javafx.fxml;
     exports org.example.onlineexchange;
